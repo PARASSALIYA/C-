656c7326e123a2649cff14f303d436f6ef87aaa4
Paras Saliya <143994351+PARASSALIYA@users.noreply.github.com>
2023-12-08 12:30:00 +0530

    6/12/2023

---
+++ practice/practice/ch-1/2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "* ";
+    cout << endl;
+    cout << "* *";
+    cout << endl;
+    cout << "* * *";
+    cout << endl;
+    cout << "* *";
+    cout << endl;
+    cout << "* ";
+}
