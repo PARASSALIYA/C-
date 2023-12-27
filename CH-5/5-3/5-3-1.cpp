8e9c9835579d9b99f876868aaa8654d86331cd54
Paras Saliya <143994351+PARASSALIYA@users.noreply.github.com>
2023-12-27 17:06:48 +0530

    27/12/2023

---
+++ CH-5/5-3/5-3-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Result
+{
+private:
+    int sub;
+
+public:
+    void sci()
+    {
+        cout << "Enter sci :";
+        cin >> sci;
+
+        cout << "sci marks :" << sci;
+    }
+    // void operator++()
+    // {
+
+    // }
+};
+
+int main()
+{
+
+    Result r1;
+    r1.sci();
+}
